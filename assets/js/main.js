2506a0f9176c18dc710316458d92848056ad52d3
lucaruboni <lucaruboni@gmail.com>
2023-03-14 16:09:51 +0100

    [add] img array [add] img loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+//creo l'array
+
+
 const images = [
     '../assets/img/01.webp',
     '../assets/img/02.webp',
@@ -7,3 +20,22 @@ const images = [
 ]
 
 console.log(images);
+
+
+let activeImage = 0 ;
+
+const imagesEl = document.querySelector('.slider > .images');
+console.log(imagesEl);
+
+//loop
+
+for (let i = 0; i < images.length; i++) {
+
+    const imgSrc = images[i];
+    const imgEl = `<img class="img-fluid ${i === activeImage ? 'active' : ''}" src="${imgSrc}" alt="">`
+
+    console.log(imgEl)
+    
+}
+
+
